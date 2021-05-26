c1dce4136df6f21cecc8bf7ba0a73c2c8dac5450
Arkadi-Dvir <73054794+Arkadi-Dvir@users.noreply.github.com>
2021-05-26 05:00:54 +0300

    Create Jacobi-Method.py

---
+++ Iterative Methods/Jacobi-Method.py
@@ -0,0 +1,15 @@
+import numpy as np
+from numpy import linalg as LA
+def jacobi(A,b,x0,num_iter):
+  n = len(A[0])
+  for k in range(num_iter):
+    y = [0] * n
+    for i in range(n):
+      sumvec = 0
+      for j in range(n):
+        if j != i:
+          sumvec = sumvec + (A[i][j] * x0[j])
+      y[i] = (1/A[i][i])*(b[i] - sumvec)
+    x0 = y
+  vec = np.dot(A,x0)
+  return (LA.norm(np.subtract(vec, b)))
